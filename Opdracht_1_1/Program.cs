cc46ef30a5e88e2c528b90189b3c209a24703710
Jesse <Jesse@JESSE-DESKTOP>
2018-01-31 23:39:59 +0100

    Design Patterns - Alle opdrachten

--- Opdracht_1_1/Program.cs
+++ Opdracht_1_1/Program.cs
@@ -10,6 +10,14 @@ namespace Opdracht_1_1
     {
         static void Main(string[] args)
         {
+            BoekHandel boekhandel = new BoekHandel();
+            boekhandel.VoegToe(new CD("Gerwin", "Gerwin zingt de sterren van de hemel", 12, 10000000));
+            boekhandel.VoegToe(new Weekblad("28 februari 1997", "ICT, daar kan je wat mee", 20.50f, 0));
+            boekhandel.VoegToe(new Boek("Sinterklaas", "Het Grote Boek van Sinterklaas", 250, 1));
+
+            boekhandel.PrintOverzicht();
+
+            Console.ReadKey();
         }
     }
 }
